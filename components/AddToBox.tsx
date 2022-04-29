d1f897b5efb1ea9bde7b8f311586eeaf1a6b809b
edehlol <edymusajev@gmail.com>
2022-04-29 13:31:17 +0200

    finished tooltip popover

--- components/AddToBox.tsx
+++ components/AddToBox.tsx
@@ -12,7 +12,9 @@ import {
   selectShippingTime,
 } from '../features/product/productSlice';
 import Cart from './Cart';
-import { Popover } from '@headlessui/react';
+import { Popover, Transition } from '@headlessui/react';
+import { usePopper } from 'react-popper';
+import { useState } from 'react';
 
 const Container = styled.div`
   padding: 28px 26px;
@@ -61,27 +63,45 @@ const Button = styled.button<ButtonProps>`
   color: ${(props) => (props.primary ? '#FFFFFF' : '#FF6600')};
 `;
 
-const PopoverButton = styled(Popover.Button)`
+const PopoverButton = styled(Popover.Button)<any>`
   border: none;
   background-color: white;
 `;
-const PopoverPanel = styled(Popover.Panel)`
+const PopoverPanel = styled(Popover.Panel)<any>`
   z-index: 20;
   position: absolute;
   background-color: white;
   width: 320px;
   box-shadow: 0 2px 8px 0 rgba(0, 0, 0, 0.05);
+  border: 1px solid #e6e7eb;
   padding: 12px;
   border-radius: 8px;
 `;
 
 const InfoPopover = ({ info }: { info: string }) => {
+  const [referenceElement, setReferenceElement] = useState(null);
+  const [popperElement, setPopperElement] = useState(null);
+  const [arrowElement, setArrowElement] = useState(null);
+  const { styles, attributes } = usePopper(referenceElement, popperElement, {
+    modifiers: [{ name: 'arrow', options: { element: arrowElement } }],
+  });
+
+  const [isShowing, setIsShowing] = useState(false);
+
   return (
     <Popover style={{ position: 'relative' }}>
-      <PopoverButton>
+      <PopoverButton
+        ref={setReferenceElement}
+        onMouseEnter={() => setIsShowing(true)}
+        onMouseLeave={() => setIsShowing(false)}
+      >
         <Image src="/icons/info.png" width="15px" height="14px" alt="info" />
       </PopoverButton>
-      <PopoverPanel>{info}</PopoverPanel>
+      <Transition show={isShowing}>
+        <PopoverPanel ref={setPopperElement} style={styles.popper} {...attributes.popper}>
+          {info}
+        </PopoverPanel>
+      </Transition>
     </Popover>
   );
 };
